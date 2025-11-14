194f6cd2c25fbf3a4493cf81a096872502d15f6e
Kiara <usr4066@salesianos-lacuesta.net>
2025-11-14 08:19:19 +0000

    refactor(card): update CardItem import path to correct nomenclature

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Image, Text, View, StyleSheet } from "react-native";
 import { lightColorPalette } from "../theme/styles";
-import { CardItem } from "../types/CardItem";
+import { CardItem } from "../types/card";
 
 const Card: React.FC<CardItem> = ({ title, imageSource, bodyText }) => {
   return (
